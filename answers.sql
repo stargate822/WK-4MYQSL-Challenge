16336ac6ab952a68150303be096e4de1d486c1e3
stargate822 <lolawy1st@gmail.com>
2025-09-09 18:28:20 +0100

    week 4 complete assignment

---
+++ answers.sql
@@ -0,0 +1,16 @@
+-- Question 1
+-- Show the total payment amount for each payment date (latest 5 dates)
+
+USE PaymentDB;
+
+-- Question: Find the average credit limit of each customer
+
+-- Question 3
+-- Find the total price of products ordered from 
+orderdetails table
+
+-- Question 4
+-- Find the highest payment amount for each check
+ number from payment
+
+
